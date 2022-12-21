fd222cf784f56d1b34a255ad744b2e4b3cb49749
Adesida Leye <adeleyeadesida@gmail.com>
2022-12-21 21:16:02 +0100

    update on js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const taskInput = document.querySelector('.task-input input');
 
+let todoList = JSON.parse(localStorage.getItem('todo-list'));//getting localStorage todo-list then parsing
+function showTodo(){
+    todoList.forEach((todo, id) =>{
+        console.log(id, todo);
+    })
+}
+
 taskInput.addEventListener('keyup', e =>{
     let taskValue = taskInput.value;
     
     if(e.key == 'Enter' && taskValue != ''){
-        console.log(taskValue)
+        if(!todoList){
+            todoList = [];
+        }
+        taskInput.value = '';
+        let todoValues = {name: taskValue, status: 'Pending'};
+        todoList.push(todoValues);//adding new todo to todoList
+        localStorage.setItem('todo-list', JSON.stringify(todoList));//saving to localStorage with name as todo-list
+        showTodo();
     }
 })
+
