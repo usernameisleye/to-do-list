1e850ffd43844b8e1fc834a1caff8cd4bce29997
Adesida Leye <adeleyeadesida@gmail.com>
2022-12-21 22:14:36 +0100

    update on files

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const taskInput = document.querySelector('.task-input input');
+const taskContent = document.querySelector('.task-content');
 
 let todoList = JSON.parse(localStorage.getItem('todo-list'));//getting localStorage todo-list then parsing
 function showTodo(){
-    todoList.forEach((todo, id) =>{
-        console.log(id, todo);
-    })
+    let li = '';
+    if(todoList){//if todoList exists
+        todoList.forEach((todo, id) =>{
+            li +=  `<li class="task">
+                    <label for="${id}">
+                        <input type="checkbox" id="${id}">
+                        <p>${todo.name}</p>
+                    </label>
+    
+                    <div class="options">
+                        <i class="fa-solid fa-ellipsis"></i>
+                        
+                        <ul class="task-options">
+                            <li><i class="fa-regular fa-pen-to-square"></i>Edit</li>
+                            <li><i class="fa-regular fa-trash-can"></i>Delete</li>
+                        </ul>
+                    </div>
+                </li>`
+        }); 
+    }
+    taskContent.innerHTML = li;
 }
+showTodo();  
 
 taskInput.addEventListener('keyup', e =>{
     let taskValue = taskInput.value;
